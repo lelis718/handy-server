62a4d1425a0773205f02449b2b8e87bb6ac9f35c
= <edsondiasalves@hotmail.com>
2020-04-10 18:44:12 +0100

    Adding firestone

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # handy-server
 Backend application for Handy Project 
+
+
+# 1 - Install dependencies
+`
+cd function
+npm install
+`
+
+## 2 - Get the service account key
+Visit the Service Accounts Page, select your project, create your key and place it at 
+functions/service-account-key.json
+
+Reference:
+https://cloud.google.com/compute/docs/access/create-enable-service-accounts-for-instances
+
+## 3 - Run the app typing the command in the root of the app
+`firebase serve`
+
+## 4 - After update your app, deploy it running the command below in the root of the app
+`firebase deploy`
--- functions/index.js
+++ functions/index.js
@@ -1,51 +1,34 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const express = require('express');
 
+let serviceAccount = require('./service-account-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello from Firebase from express!");
 });
 
-
 app.get('/cards', (req, res) => {
-    res.send([
-            {
-                message: "Welcome to Handy!",
-                icon: "smileBeam", 
-                color: "lightBlueAccent",
-            },
-            {
-                message: "Need some help?", 
-                icon: "questionCircle", 
-                color: "lightBlueAccent"
-            },
-            {
-                message: "Add local and what you are planning to do", 
-                icon: "searchLocation", 
-                color: "lightBlueAccent"
-            },
-            {
-                message: "Someone will receive your request to give you a hand!", 
-                icon: "smileWink", 
-                color: "lightBlueAccent"
-            },
-            {
-                message: "Want to help someone?", 
-                icon: "peopleCarry", 
-                color: "lightBlueAccent"
-            },
-            {
-                message: "Swipe between the cards for give a hand to someone.", 
-                icon: "arrowsAltH", 
-                color:  "lightBlueAccent"
-            },
-            {
-                message: "Shall we begin?", 
-                icon: "smileBeam", 
-                color: "lightBlueAccent"
-            },
-        ]);
+    let cards = [];
+    db.collection('cards').get()
+        .then((snapshot) => {
+            console.log(snapshot);
+            snapshot.forEach((doc) => {
+                cards.push(doc.data());
+            });
+
+            res.send(cards);
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err);
+        });
     }
 );
 
